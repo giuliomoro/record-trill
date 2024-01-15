9ad8e954585960ad462c03084e89318e0b6154f2
Giulio Moro <giuliomoro@yahoo.it>
2024-01-15 17:27:00 -0500

    c++ for bar

--- main.cpp
+++ main.cpp
@@ -23,49 +23,57 @@ const size_t kMaxNumPads = 0;
 #endif // LOG_RAW
 struct Frame { double timestamp; float position; float size; float pads[kMaxNumPads]; };
 
-float gNumFrames = 5000;
+float gNumFrames = 3000;
 
 int main()
 {
 	// pre-allocate all the memory needed to store the audio data
 	struct Frame oldFrame = {0};
-	unsigned int bits = 10;
-	unsigned int pres = 2;
-	unsigned int noiseThreshold = 40; // 40 is the default value in firmware
+	unsigned int bits = 12;
+	unsigned int pres = 3;
+	unsigned int noiseThreshold = 40; // the sensor is always set to 0, this is used only to pre-process the data for touch detection
+	size_t constexpr kNumPads = 26;
 #ifdef LOG_RAW
 	unsigned int multBits = 11;
 #endif // LOG_RAW
 	std::vector<struct Frame> inputs(gNumFrames);
 	Trill trill;
-	if(trill.setup(1, Trill::RING))
+	if(trill.setup(1, Trill::BAR))
 		return 1;
 	usleep(10000);
 	trill.setScanSettings(0, bits);
 	usleep(10000);
 	trill.setPrescaler(pres);
 	usleep(10000);
-	trill.updateBaseline();
+	trill.setTimerPeriod(1);
+	usleep(10000);
 #ifdef LOG_RAW
 	trill.setMode(Trill::DIFF);
-	CentroidDetection cent(28, 5, 1); // Ring has 28 pads
-	cent.setNoiseThreshold(noiseThreshold / float(1 << bits)); // have to convert as if it were the value that is passed to Trill::setNoiseThreshold
-	cent.setWrapAround(5); // consider up to 5 pads when wrapping around
+	usleep(10000);
+	trill.setScanTrigger(Trill::kScanTriggerTimer);
+	usleep(10000);
+	CentroidDetection cent(kNumPads, 5, 1);
+	cent.setNoiseThreshold(0);
 	cent.setMultiplierBits(multBits);
 #endif // LOG_RAW
 	auto start = std::chrono::steady_clock::now();
+	std::vector<float> removeNoise;
 	for(unsigned int n = 0; n < inputs.size(); ++n)
 	{
 		struct Frame frame;
 		while(1)
 		{
-			// read as fast as possible but only keep the frame if different from the previous one
+			// read frequently but only keep the frame if different from the previous one
 			// (empirical workaround to only take new readings)
 			if(trill.readI2C())
 			{
 				return 1;
 			}
 #ifdef LOG_RAW
-			cent.process(trill.rawData.data());
+			removeNoise = trill.rawData;
+			for(auto& r : removeNoise) // remove noise threshold and clip to 0
+				r = std::max(0.f, r - noiseThreshold / float(1 << bits));  // have to convert as if it were the value that is passed to Trill::setNoiseThreshold
+			cent.process(removeNoise.data());
 			auto& touches = cent;
 #else
 			auto& touches = trill;
@@ -88,7 +96,7 @@ int main()
 				oldFrame.size = frame.size;
 				break;
 			}
-			usleep(100);
+			usleep(1000);
 		}
 		inputs[n].position = frame.position;
 		inputs[n].size = frame.size;
@@ -104,7 +112,7 @@ int main()
 #ifdef LOG_RAW
 	type = "_raw_mult" + std::to_string(multBits);
 #endif // LOG_RAW
-	std::string filename = "ring" + type + "_bits" + std::to_string(bits) + "_pres" + std::to_string(pres) + "_thr" + std::to_string(noiseThreshold) + ".m";
+	std::string filename = "bar" + type + "_bits" + std::to_string(bits) + "_pres" + std::to_string(pres) + "_thr" + std::to_string(noiseThreshold) + ".m";
 	printf("Stopped, writing '%s'\n", filename.c_str());
 	std::string out = "data =[\n";
 	for(auto& f : inputs)
